c7d56f6594ec4f9158dddf9fd9f2cac55e4ac6f4
Ashlyn Joshy <ashlyntherese@gmail.com>
2024-08-15 17:57:57 +0530

    tailwind setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to Book Generator API</h1>
+      <h1 className="text-2xl font-bold">Welcome to Book Generator API</h1>
     </div>
   );
 }
